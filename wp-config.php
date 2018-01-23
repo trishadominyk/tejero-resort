67d39919889a2d261cfe09c5c5d0d34a28b82c84
Trisha Alvarez <trishadominyk@github.com>
2018-01-23 16:52:10 +0800

    Second update

--- wp-config.php
+++ wp-config.php
@@ -37,7 +37,7 @@ define('DB_CHARSET', 'utf8mb4');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-define('WP_MEMORY_LIMIT', '256M' );
+define('WP_MEMORY_LIMIT', '400M' );
 
 /**#@+
  * Authentication Unique Keys and Salts.
